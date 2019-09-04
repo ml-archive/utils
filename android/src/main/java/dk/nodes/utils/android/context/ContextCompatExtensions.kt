7b9def4d5cef424d144af7facb737afbdcd6e8f0
maru <maru@nodesagency.com>
2019-09-04 08:40:43 +0200

    Change fallback color to transparent

--- android/src/main/java/dk/nodes/utils/android/context/ContextCompatExtensions.kt
+++ android/src/main/java/dk/nodes/utils/android/context/ContextCompatExtensions.kt
@@ -1,11 +1,17 @@
 package dk.nodes.utils.android.context
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 
-fun ContextCompat.getNonNullDrawable(context: Context, @DrawableRes id: Int): Drawable {
-    return ContextCompat.getDrawable(context, id) ?: ColorDrawable()
+fun ContextCompat.getNonNullDrawable(
+    context: Context,
+    @DrawableRes id: Int,
+    @ColorInt fallbackColor: Int = Color.TRANSPARENT
+): Drawable {
+    return ContextCompat.getDrawable(context, id) ?: ColorDrawable(fallbackColor)
 }
