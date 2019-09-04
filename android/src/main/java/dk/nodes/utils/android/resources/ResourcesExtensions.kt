945ca33ef0ba3d8469cc5dc42bdc2510ec033549
maru <maru@nodesagency.com>
2019-09-04 08:32:49 +0200

    Add scaledDensity

--- android/src/main/java/dk/nodes/utils/android/resources/ResourcesExtensions.kt
+++ android/src/main/java/dk/nodes/utils/android/resources/ResourcesExtensions.kt
@@ -6,3 +6,7 @@ import kotlin.math.roundToInt
 val Int.dp: Int get() = (this * Resources.getSystem().displayMetrics.density).roundToInt()
 
 val Float.dp: Float get() = (this * Resources.getSystem().displayMetrics.density)
+
+val Int.sp: Int get() = (this * Resources.getSystem().displayMetrics.scaledDensity).roundToInt()
+
+val Float.sp: Float get() = (this * Resources.getSystem().displayMetrics.scaledDensity)
